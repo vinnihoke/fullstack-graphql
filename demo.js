406e8e800e1e3098cb298cd0bf5f0cdb2eeca2ae
Vinni Hoke <vinni.hoke@gmail.com>
2020-01-14 22:55:45 -0800

    Stopping at Query Types Exercise

--- demo.js
+++ demo.js
@@ -1,7 +1,8 @@
 const gql = require('graphql-tag');
 const { ApolloServer } = require('apollo-server')
 
-
+// Don't modify the schema on the fly. Spend time and define the schema with your team.
+// You must have typeDef, a query, and a resolver. This is everything you need to run a server.
 const typeDefs = gql`
 	type User {
 		email: String!
@@ -15,6 +16,8 @@ const typeDefs = gql`
 `
 
 const resolvers = {
+	// These must match the typeDefs exactly, otherwise they won't run.
+	// These can actually do anything you'd like. For instance, you can have these return information from an API, or run a database query. You could have custom things happening in the resolver... As long as they are returned in the proper shape of the typeDefs.
 	Query: {
 		me() {
 			return {
@@ -37,5 +40,14 @@ The query doesn't care how you get the data, as long as the data is in the shape
 
 What is a resolver?
 Functions that are responsible for returning values for fields that exist on Types in a Schoma. resolvers execution is dependent on the incoming client Query.
+
+The query that is requested by the client is executed by the resolvers.
+
+{
+	I'm executing the me query, and firing off the me resolver. This could then grab the information from whichever database you're using.
+	me {
+		email
+	}
+}
 
 */
