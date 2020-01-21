b99c388585ca6595aa81e43fd4239cbf7c259041
Vinni Hoke <vinni.hoke@gmail.com>
2020-01-21 09:57:46 -0800

    Mutations

--- api/src/db/user.js
+++ api/src/db/user.js
@@ -8,10 +8,10 @@ const createUserModel = db => {
     },
 
     create(user) {
-      const newUser = {id: nanoid(), createdAt: Date.now(), ...user}
+      const newUser = { id: nanoid(), createdAt: Date.now(), ...user }
       db.set('user', newUser)
         .write()
-  
+
       return newUser
     }
   }
--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -2,22 +2,33 @@
  * Here are your Resolvers for your Schema. They must match
  * the type definitions in your scheama
  */
+// Don't pass in the models directly. Although it will work, it will make testing more difficult.
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_, { input }, { models }) {
+      return models.Pet.findMany(input)
+    },
+    pet(_, { input }, { models }) {
+      return models.Pet.findOne(input)
+    },
+    shoes(_, { input }) {
+      return [{ brand: 'nike', size: 12 }, { brand: 'adidas', size: 11 }].filter(shoe =>
+        shoe.brand === input.brand
+      )
     }
   },
-  User: {
-    
-  }
+  // Mutation: {
+
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+
+  // }
 }
